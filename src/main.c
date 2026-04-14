62152efd3aec92cbf0494bb5a31049adca6c1f8b
Peter Liao <peterliao2004@gmail.com>
2026-04-14 13:14:12 +1000

    Loaded in Game example as base structure to build up from

---
+++ src/main.c
@@ -0,0 +1,155 @@
+/*
+ * example game task main
+ *
+ * This file is meant to be a starting point for your game.  It includes the
+ * basic setup for a FreeRTOS application, including the creation of a task and
+ * a timer interrupt.  You will need to add additional tasks in the game_tasks.c file.
+ * 
+ */
+
+/* Standard includes. */
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdint.h>
+
+/* Kernel includes. */
+#include "FreeRTOS.h"
+#include "task.h"
+#include "semphr.h"
+
+/* Hardware includes. */
+#include "inc/hw_memmap.h"
+#include "inc/hw_sysctl.h"
+#include "driverlib/interrupt.h"
+#include "driverlib/rom.h"
+#include "driverlib/rom_map.h"
+#include "driverlib/sysctl.h"
+#include "drivers/rtos_hw_drivers.h"
+/*-----------------------------------------------------------*/
+
+/* The system clock frequency. */
+volatile uint32_t g_ui32SysClock;
+
+/* Global for binary semaphore shared between tasks. */
+SemaphoreHandle_t xGameUpdateSemaphore = NULL;
+SemaphoreHandle_t xGameStartSemaphore = NULL;
+SemaphoreHandle_t xDisplaySemaphore = NULL;
+
+/* Set up the clock and pin configurations to run this example. */
+static void prvSetupHardware( void );
+
+/* function to create the game display task. */
+extern void vCreateDisplayTask( void );
+/*-----------------------------------------------------------*/
+
+int main( void )
+{
+    /* Prepare the hardware to run this example. */
+    prvSetupHardware();
+
+    /* Create the binary semaphore used to synchronize the button ISR and the
+     * button processing task. */
+    xGameUpdateSemaphore = xSemaphoreCreateBinary();
+    xGameStartSemaphore = xSemaphoreCreateBinary();
+    xDisplaySemaphore = xSemaphoreCreateBinary();
+
+    if ( (xGameUpdateSemaphore != NULL) && (xGameStartSemaphore != NULL) && (xDisplaySemaphore != NULL) )
+    {
+        /* Configure application specific hardware and initialize the tasks. */
+        vCreateDisplayTask();
+
+        /* you should add a second create task for game logic here*/
+
+        /* Start the tasks. */
+        vTaskStartScheduler();
+    }
+
+    /* If all is well, the scheduler will now be running, and the following
+    line will never be reached.  If the following line does execute, then
+    there was insufficient FreeRTOS heap memory available for the idle and/or
+    timer tasks to be created.  See the memory management section on the
+    FreeRTOS web site for more details. */
+    for( ;; );
+}
+/*-----------------------------------------------------------*/
+
+static void prvSetupHardware( void )
+{
+    /* Run from the PLL at configCPU_CLOCK_HZ MHz. */
+    g_ui32SysClock = MAP_SysCtlClockFreqSet((SYSCTL_XTAL_25MHZ |
+            SYSCTL_OSC_MAIN | SYSCTL_USE_PLL |
+            SYSCTL_CFG_VCO_240), configCPU_CLOCK_HZ);
+
+    /* Configure device pins. */
+    // This sets most pins to their default use
+    // When using for a different project ensure to configure
+    // project specific pins/devices after this function
+    PinoutSet(false, false);
+}
+/*-----------------------------------------------------------*/
+
+void vApplicationMallocFailedHook( void )
+{
+    /* vApplicationMallocFailedHook() will only be called if
+    configUSE_MALLOC_FAILED_HOOK is set to 1 in FreeRTOSConfig.h.  It is a hook
+    function that will get called if a call to pvPortMalloc() fails.
+    pvPortMalloc() is called internally by the kernel whenever a task, queue,
+    timer or semaphore is created.  It is also called by various parts of the
+    demo application.  If heap_1.c or heap_2.c are used, then the size of the
+    heap available to pvPortMalloc() is defined by configTOTAL_HEAP_SIZE in
+    FreeRTOSConfig.h, and the xPortGetFreeHeapSize() API function can be used
+    to query the size of free heap space that remains (although it does not
+    provide information on how the remaining heap might be fragmented). */
+    IntMasterDisable();
+    for( ;; );
+}
+/*-----------------------------------------------------------*/
+
+void vApplicationIdleHook( void )
+{
+    /* vApplicationIdleHook() will only be called if configUSE_IDLE_HOOK is set
+    to 1 in FreeRTOSConfig.h.  It will be called on each iteration of the idle
+    task.  It is essential that code added to this hook function never attempts
+    to block in any way (for example, call xQueueReceive() with a block time
+    specified, or call vTaskDelay()).  If the application makes use of the
+    vTaskDelete() API function (as this demo application does) then it is also
+    important that vApplicationIdleHook() is permitted to return to its calling
+    function, because it is the responsibility of the idle task to clean up
+    memory allocated by the kernel to any task that has since been deleted. */
+}
+/*-----------------------------------------------------------*/
+
+void vApplicationStackOverflowHook( TaskHandle_t pxTask, char *pcTaskName )
+{
+    ( void ) pcTaskName;
+    ( void ) pxTask;
+
+    /* Run time stack overflow checking is performed if
+    configCHECK_FOR_STACK_OVERFLOW is defined to 1 or 2.  This hook
+    function is called if a stack overflow is detected. */
+    IntMasterDisable();
+    for( ;; );
+}
+/*-----------------------------------------------------------*/
+
+void *malloc( size_t xSize )
+{
+    /* There should not be a heap defined, so trap any attempts to call
+    malloc. */
+    IntMasterDisable();
+    for( ;; );
+}
+/*-----------------------------------------------------------*/
+
+void vApplicationTickHook( void )
+{
+    /* This function will be called by each tick interrupt if
+        configUSE_TICK_HOOK is set to 1 in FreeRTOSConfig.h.  User code can be
+        added here, but the tick hook is called from an interrupt context, so
+        code must not attempt to block, and only the interrupt safe FreeRTOS API
+        functions can be used (those that end in FromISR()). */
+
+    /* Only the full demo uses the tick hook so there is no code is
+        executed here. */
+}
+/*-----------------------------------------------------------*/
